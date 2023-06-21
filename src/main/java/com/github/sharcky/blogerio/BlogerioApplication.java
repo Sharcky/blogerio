f2a8aefb88eec976de202661dff682b5876a59e4
Miguel Pierre Joseph <56655360+Sharcky@users.noreply.github.com>
2023-06-21 14:53:29 -0300

    Add DEBUG and INFO log

--- src/main/java/com/github/sharcky/blogerio/BlogerioApplication.java
+++ src/main/java/com/github/sharcky/blogerio/BlogerioApplication.java
@@ -1,13 +1,22 @@
 package com.github.sharcky.blogerio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class BlogerioApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BlogerioApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogerioApplication.class, args);
+	}
+
+	public void runner(String... args){
+		LOGGER.info("Application has been stated!");
 	}
 
 }
